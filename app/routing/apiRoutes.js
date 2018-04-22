9e3607c5012e75a0a6ba9de0038122e27df6691a
Christian Lopes <christianlopes@Christian-Lopess-iMac-2.local>
2018-04-21 21:26:45 -0700

    all routes functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,35 @@
+//Pull in required dependencies
 var path = require('path');
 
+//Import the friends list
+var friends = require('../data/friends.js');
 
-module.exports = function(app) {
+//Export the API routes so that they're avaialble outside of this file
+module.exports = function (app) {
+
+
+    //Displays friends list
+    app.get('/api/friends', function (req, res) {
+        res.json(friends)
+    })
+
+    //Post new entry to the friends array
+    app.post('/api/friends', function (req, res) {
+        friends.push(req.body)
+
+          //console.log('req', req);
+  var userData = req.body; // where the body parser shines
+  console.log('userData', userData);
+
+  // loop over friends and find best match
+  for (var i = 0; i < friends.length; i++) {
+    console.log("console logging i" + i)
+  }
+
+  // add new friend to friends array
+
+  // return best match
+  res.json(friends);
+    })
 
 }
--- server.js
+++ server.js
@@ -11,79 +11,11 @@ app.use(express.static(path.join(__dirname, 'app/public')))
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
-var friends = [
-  {
-    name: "Bryan Tran",
-    photo: "No Image",
-    scores: [
-      "5",
-      "1",
-      "4",
-      "4",
-      "5",
-      "1",
-      "2",
-      "5",
-      "4",
-      "1"
-    ]
-  },
-  {
-    name: "Quinton Fults",
-    photo: "No Image",
-    scores: [
-      "4",
-      "2",
-      "5",
-      "1",
-      "3",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2"
-    ]
-  },
-];
-
-var htmlRoutes = require('./app/routing/htmlRoutes')(app);
 //htmlRoutes(app);
+var htmlRoutes = require('./app/routing/htmlRoutes')(app);
 
 //This is new - testing
-var apiRoutes = require('./app/routing/apiRoutes')(app);
-
-/*app.get('/', function (req, res) {
-//  res.sendFile('home.html')
-res.sendFile('app/public/home.html', {root: __dirname}) 
-})
-
-app.get('/survey', function (req, res) {
-  //  res.sendFile('home.html')
-  res.sendFile('app/public/survey.html', {root: __dirname}) 
-})*/
-
-app.get('/api/friends', function (req, res) {
-  res.json(friends)
-});
-
-app.post('/api/friends', function (req, res) {
-  friends.push(req.body)
-
-  //console.log('req', req);
-  var userData = req.body; // where the body parser shines
-  console.log('userData', userData);
-
-  // loop over friends and find best match
-  for (var i = 0; i < friends.length; i++) {
-    console.log("console logging i" + i)
-  }
-
-  // add new friend to friends array
-
-  // return best match
-  res.json(friends);
-
-});
+çrequire('./app/routing/apiRoutes')(app);
 
 var PORT = process.env.PORT || 3000
 // listening port
